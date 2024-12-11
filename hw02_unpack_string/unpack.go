7b392a72e0e587183f5d74cd1b6216a9f898b2cd
Evgeny Kozhin <ekozhin@axilon.ru>
2024-12-11 19:47:59 +0300

    HW2 is compiled

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,57 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	runeString := []rune(s)
+	var resultString strings.Builder
+	isMask := false
+
+	for index, char := range runeString {
+		if index == 0 {
+			if isDigit(char) {
+				return "", ErrInvalidString
+			}
+		}
+
+		if isMask {
+			resultString.WriteRune(char)
+			isMask = false
+			continue
+		}
+
+		if char == '\\' {
+			isMask = true
+			continue
+		}
+
+		if num, err := strconv.Atoi(string(char)); err == nil {
+			if index == 0 || index < len(runeString)-1 && isDigit(runeString[index+1]) {
+				return "", ErrInvalidString
+			}
+			if num > 0 {
+				resultString.WriteString(strings.Repeat(string(runeString[index-1]), num-1))
+			} else {
+				tmp := resultString.String()
+				resultString.Reset()
+				resultString.WriteString(tmp[:len(tmp)-1])
+			}
+		} else {
+			resultString.WriteRune(char)
+		}
+	}
+
+	return resultString.String(), nil
+}
+
+func isDigit(r rune) bool {
+	if _, err := strconv.Atoi(string(r)); err == nil {
+		return true
+	}
+	return false
 }
