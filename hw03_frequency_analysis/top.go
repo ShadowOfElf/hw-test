12b579ca66300bbda9d3e445cda4373eb1b006d0
Evgeny Kozhin <ekozhin@axilon.ru>
2024-12-15 11:44:05 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(inputStr string) []string {
+	if len(inputStr) == 0 {
+		return nil
+	}
+
+	freqMap := map[string]int{}
+	for _, word := range strings.Fields(inputStr) {
+		freqMap[word]++
+	}
+
+	type freqStruct struct {
+		word string
+		freq int
+	}
+
+	resultSlice := make([]freqStruct, len(freqMap))
+	i := 0
+	for word, count := range freqMap {
+		resultSlice[i] = freqStruct{word: word, freq: count}
+		i++
+	}
+
+	sort.Slice(resultSlice, func(i, j int) bool {
+		if resultSlice[i].freq == resultSlice[j].freq {
+			return resultSlice[i].word < resultSlice[j].word
+		}
+		return resultSlice[i].freq > resultSlice[j].freq
+	})
+
+	resultStrSlice := make([]string, 0, len(resultSlice))
+
+	for index, freqEl := range resultSlice {
+		if index > 9 {
+			break
+		}
+		resultStrSlice = append(resultStrSlice, freqEl.word)
+	}
+
+	return resultStrSlice
 }
