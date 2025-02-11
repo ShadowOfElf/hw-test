121560e99d93e25544b9a993f78a980538798bca
Evgeny Kozhin <ekozhin@axilon.ru>
2025-02-11 09:07:17 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env := Environment{
+		"BAR":   EnvValue{"bar", false},
+		"EMPTY": EnvValue{"", true},
+		"FOO":   EnvValue{"   foo\nwith new line", false},
+		"HELLO": EnvValue{"\"hello\"", false},
+		"UNSET": EnvValue{"", true},
+	}
+	t.Run("correct_run", func(t *testing.T) {
+		args := []string{"/bin/bash", "./testdata/echo.sh", "arg1=1", "arg2=2"}
+		code := RunCmd(args, env)
+		require.Equal(t, 0, code)
+	})
 }
